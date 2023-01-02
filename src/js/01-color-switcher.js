ad74792d8aff2c537f1a8f292ff83ff776d5ae2e
OlenaMurza <murzaelena@gmail.com>
2023-01-02 20:49:05 +0200

    add

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,25 @@
+
+const tachStart = document.querySelector('button[data-start]');
+const tachStop = document.querySelector('button[data-stop]');
+
+tachStart.addEventListener('click', changeColor);
+tachStop.addEventListener('click', onBtnStop);
+
+const INTERVAL_DELAY = 1000;
+let intervalId = null;
+
+ function getRandomHexColor() {
+        return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+    }
+
+function  changeColor() {
+    intervalId = setInterval(() => {
+    document.body.style.backgroundColor = colors[getRandomHexColor(0, colors.length - 1)];
+    }, INTERVAL_DELAY);
+    tachStart.disabled = true;
+};
+
+function onBtnStop() {
+    clearInterval(intervalId);
+    tachStart.disabled = false;
+}
