6ecc1d408203dc617623091721a315ce9c0a0dfe
OlenaMurza <murzaelena@gmail.com>
2023-01-10 20:12:28 +0200

    add

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,19 +7,22 @@ touchStop.addEventListener('click', onBtnStop);
 
 const INTERVAL_DELAY = 1000;
 let intervalId = null;
+touchStop.disabled = true;
 
  function getRandomHexColor() {
         return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
     }
 
-function  changeColor() {
+function changeColor() {
     intervalId = setInterval(() => {
     document.body.style.backgroundColor = getRandomHexColor();
     }, INTERVAL_DELAY);
     touchStart.disabled = true;
+    touchStop.disabled = false;
 };
 
 function onBtnStop() {
     clearInterval(intervalId);
     touchStart.disabled = false;
+    touchStop.disabled = true;
 }
