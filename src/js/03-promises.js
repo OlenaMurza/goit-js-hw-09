0942f09a727d5dd37f89180c3b3c21a3791ecd14
OlenaMurza <murzaelena@gmail.com>
2023-01-11 20:25:42 +0200

    add

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -14,17 +14,16 @@ function onSubmitForm(event) {
   for (let position = 1; position <= amount; position += 1) {
     createPromise(position, delay)
       .then(({ position, delay }) => {
-        setTimeout(() => {
-          Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { useIcon: false });
-        }, delay);
+        
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`, { useIcon: false });
       })
+      
       .catch(({ position, delay }) => {
-        setTimeout(() => {
-          Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, { useIcon: false });
-        }, delay);
-      });
-    delay += step;
-  }
+       
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`, { useIcon: false });
+      }
+      
+      ); delay += step;  }
 }
 
 function createPromise(position, delay) {
@@ -32,9 +31,10 @@ function createPromise(position, delay) {
   const objectPromise = { position, delay };
 
   return new Promise((resolve, reject) => {
+    setTimeout(() => {
     if (shouldResolve) {
       resolve(objectPromise);
     }
     reject(objectPromise);
-  });
+  });}, delay);
 }
